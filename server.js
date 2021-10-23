70feab1fce2ae17ed9d858a7d6e32499f07d3e12
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-23 10:57:45 +0300

    mongoDb connection setted up

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get("/", (res, req) => res.send("Hello"));
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//connect to MongoDB
+// mongoose
+//     .connect(db)
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err=>console.log(err))
+mongoose.connect(db,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true   
+}).then(() => console.log('Connected to MongoDB'))
+app.get('/', (res, req) => res.end("Hello"));
 
 const port = process.env.PORT || 3000;
 
