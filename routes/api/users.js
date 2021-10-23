33a3980854af91f1a49fde45f67c37f360fada0f
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-23 13:23:32 +0300

    feat: user jwt authentication is working.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-// const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys');
-// const passport = require('passport');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
 
 // Load Input Validation
 // const validateRegisterInput = require('../../validation/register');
@@ -85,21 +86,21 @@ router.post("/login", (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then((isMatch) => {
       if (isMatch) {
-        res.json({ msg: "success" });
+        // res.json({ msg: "success" });
         // User Matched
-        // const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
         // Sign Token
-        // jwt.sign(
-        //   payload,
-        //   keys.secretOrKey,
-        //   { expiresIn: 3600 },
-        //   (err, token) => {
-        //     res.json({
-        //       success: true,
-        //       token: 'Bearer ' + token
-        //     });
-        //   }
-        // );
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ error: "Password incorrect" });
       }
@@ -110,16 +111,16 @@ router.post("/login", (req, res) => {
 // @route   GET api/users/current
 // @desc    Return current user
 // @access  Private
-// router.get(
-//   '/current',
-//   passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     res.json({
-//       id: req.user.id,
-//       name: req.user.name,
-//       email: req.user.email
-//     });
-//   }
-// );
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const app = express();
-
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //connect to MongoDB
 // mongoose
 //     .connect(db)
 //     .then(() => console.log('Connected to MongoDB'))
 //     .catch(err=>console.log(err))
-mongoose.connect(db,{
+mongoose
+  .connect(db, {
     useUnifiedTopology: true,
-    useNewUrlParser: true   
-}).then(() => console.log('Connected to MongoDB'))
-app.get('/', (res, req) => res.end("Hello"));
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB"));
+app.get("/", (res, req) => res.end("Hello"));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 3000;
 
