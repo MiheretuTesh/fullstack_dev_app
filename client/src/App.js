b45f0bbf330eedfc8eb80855112500e447b1cfec
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-25 09:49:54 +0300

    feat: Signup and Login route is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-import Navbar from './components/layouts/Nabar'
-import Footer from './components/layouts/Footer'
-import Landing from './components/layouts/Landing'
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Nabar";
+import Footer from "./components/layouts/Footer";
+import Landing from "./components/layouts/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
