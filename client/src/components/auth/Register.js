4985e1bd7e26547d78b7c253267e9737e677d839
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-25 10:37:08 +0300

    feat: register state is set up

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -31,17 +31,17 @@ class Register extends Component {
 
   render() {
     return (
-      <div class="register">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 m-auto">
-              <h1 class="display-4 text-center">Sign Up</h1>
-              <p class="lead text-center">Create your DevConnector account</p>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnector account</p>
               <form action="create-profile.html" onSubmit={this.onSubmit}>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="text"
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
@@ -49,41 +49,41 @@ class Register extends Component {
                     required
                   />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="email"
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
-                  <small className="form-text text-muted">
+                  <small classNameName="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
                   </small>
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="password"
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <input
                     type="password"
-                    class="form-control form-control-lg"
+                    className="form-control form-control-lg"
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
                 </div>
-                <input type="submit" class="btn btn-info btn-block mt-4" />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
           </div>
