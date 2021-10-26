35242faa6529db109aa708bdaab40244d7b34e75
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-26 16:05:48 +0300

    feat: login functionality is working, checking if the user is still logged in and authorized.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 
 import Navbar from "./components/layouts/Nabar";
 import Footer from "./components/layouts/Footer";
@@ -9,6 +12,15 @@ import Landing from "./components/layouts/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+//check fro the token
+if (localStorage.jwtToken) {
+  // Set auth token to auth
+  setAuthToken(localStorage.jwtToken);
+  //Decode token an get user info ans exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthorized
+  store.dispatch(setCurrentUser(decoded));
+}
 function App() {
   return (
     <Provider store={store}>
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,35 +1,50 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+import { loginUser } from "../../actions/authActions";
 class Login extends Component {
   constructor() {
     super();
     this.state = {
       email: "",
       password: "",
+      errors: {},
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
+    const userData = {
       email: this.state.email,
       password: this.state.password,
     };
 
-    console.log(user);
-    this.setState({
-      email: "",
-      password: "",
-    });
+    this.props.loginUser(userData);
   }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     const { errors } = this.state;
     return (
@@ -41,7 +56,7 @@ class Login extends Component {
               <p className="lead text-center">
                 Sign in to your DevConnector account
               </p>
-              <form action="dashboard.html" onSubmit={this.onSubmit}>
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="email"
@@ -83,7 +98,7 @@ class Login extends Component {
 }
 
 Login.propTypes = {
-  registerUser: PropTypes.func.isRequired,
+  loginUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
 };
@@ -93,4 +108,4 @@ const mapStateToProps = (state) => ({
   errors: state.errors,
 });
 
-export default connect(null, { loginUser })(Login);
+export default connect(mapStateToProps, { loginUser })(Login);
