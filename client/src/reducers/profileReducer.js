9b619f00f0847de134227c59b4f528f6489a2669
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-28 06:00:26 +0300

    feat: Profiles page is created.

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,30 +1,40 @@
-import { PROFILE_LOADING, GET_PROFILE, CLEAR_CURRENT_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE
+} from '../actions/types';
 
 const initialState = {
   profile: null,
   profiles: null,
-  loading: false,
+  loading: false
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case PROFILE_LOADING:
       return {
         ...state,
-        loading: true,
+        loading: true
       };
     case GET_PROFILE:
       return {
         ...state,
-        loading: false,
         profile: action.payload,
+        loading: false
       };
-      case CLEAR_CURRENT_PROFILE:
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: action.payload,
+        loading: false
+      };
+    case CLEAR_CURRENT_PROFILE:
       return {
         ...state,
         profile: null
       };
-
     default:
       return state;
   }
