d9d53e659f23c3e5891509f7760631df75584f2f
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-28 07:39:56 +0300

    fix: Profile Compients has been fixed.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { Link } from "redux";
 import { getProfileByHandle } from "../../actions/profileActions";
+import { Link } from "react-router-dom";
 
 import Spinner from "../common/Spinner";
 import ProfileHeader from "./ProfileHeader";
@@ -17,12 +17,43 @@ class Profile extends Component {
     }
   }
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              {/* <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link> */}
+              <Link></Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername} />
+          ) : null}
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <ProfileHeader />
-        <ProfileAbout />
-        <ProfileCreds />
-        <ProfileGithub />
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
